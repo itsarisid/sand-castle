71c8562616168203ea5a492ae9a1ef2091968b3d
Sajid Khan <khanalex301@gmail.com>
2025-09-28 08:26:10 +0300

    fix errors

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { WelcomeCardComponent } from "./widgets/welcome-card/welcome-card.component";
 import { DetailsComponent } from './widgets/details/details.component';
 import { details } from './../../shared/data/dashboard/default';
-import { VisitorsChartComponent } from './widgets/visitors-chart/visitors-chart.component';
 @Component({
   selector: 'app-dashboard',
-  imports: [WelcomeCardComponent, DetailsComponent,VisitorsChartComponent],
+  imports: [WelcomeCardComponent, DetailsComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
